b963600fa58ff5a0990c9ac4973ae98085a6933f
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail..com>
2013-11-10 22:55:24 +0400

    add help book

---
+++ cookbooks/hello/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: hello
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+directory "/hello-test" do
+  owner     "root"
+  group     'root'
+  mode      0777
+  recursive true
+end
