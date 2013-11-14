5670cd999aebc408d6c353a98f4b67dbd90eaa02
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-11-15 01:47:59 +0400

    infra::create-vm support multiple machines

--- cookbooks/infra/attributes/default.rb
+++ cookbooks/infra/attributes/default.rb
@@ -1,11 +1,12 @@
 
 default[:kvm] = {
-  :name => 'chef-kvm.dan.lan',
-  :mem  => 512,
-  :template_disk => '/work/libvirt-template-guest.img',
-  :disk => {
-    :path => '/work/libvirtd/images',
-    :name => 'chef-kvm.img'
-  },
-  :mac => '52:00:00:00:00:01',
+  'chef-kvm.dan.lan' => {
+    :mem  => 512,
+    :template_disk => '/work/libvirt-template-guest.img',
+    :disk => {
+      :path => '/work/libvirtd/images',
+      :name => 'chef-kvm.img'
+    },
+    :mac => '52:00:00:00:00:01'
+  }
 }
--- cookbooks/infra/recipes/create-vm.rb
+++ cookbooks/infra/recipes/create-vm.rb
@@ -1,20 +1,22 @@
 
 
+node.kvm.each do |name,info|
+  # copy image
+  remote_file "#{ info.disk.path }/#{ info.disk.name }" do
+    source "file://#{ info.template_disk }"
+    action :create_if_missing
+  end
 
-# copy image
-remote_file "#{ node.kvm.disk.path }/#{ node.kvm.disk.name }" do
-  source "file://#{ node.kvm.template_disk }"
-  action :create_if_missing
-end
+  # create domain spec
+  template "#{Chef::Config[:file_cache_path]}/libvirt-domain-for-#{ name }.xml" do
+    source "libvirt-domain.xml.erb"
+    variables ({ :name => name, :info => info })
+    action :create
+  end
 
-# create domain spec
-template "#{Chef::Config[:file_cache_path]}/libvirt-domain-for-#{ node.kvm.name }.xml" do
-  source "libvirt-domain.xml.erb"
-  action :create
-end
-
-# call virsh
-execute "create_domain" do
-  command "virsh create --file #{Chef::Config[:file_cache_path]}/libvirt-domain-for-#{ node.kvm.name }.xml"
-  not_if "virsh domid --domain #{ node.kvm.name }"
+  # call virsh
+  execute "create_domain" do
+    command "virsh create --file #{Chef::Config[:file_cache_path]}/libvirt-domain-for-#{ name }.xml"
+    not_if "virsh domid --domain #{ name }"
+  end
 end
