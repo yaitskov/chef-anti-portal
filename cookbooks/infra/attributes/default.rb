cea145557adcead19db9fe61362be51123e83126
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-11-15 02:10:08 +0400

    add n4 and n5 machines

--- cookbooks/infra/attributes/default.rb
+++ cookbooks/infra/attributes/default.rb
@@ -8,5 +8,23 @@ default[:kvm] = {
       :name => 'chef-kvm.img'
     },
     :mac => '52:00:00:00:00:01'
+  },
+  'n4' => {
+    :mem  => 512,
+    :template_disk => '/work/libvirt-template-guest.img',
+    :disk => {
+      :path => '/work/libvirtd/images',
+      :name => 'n4.img'
+    },
+    :mac => '52:00:00:00:00:02'
+  },
+  'n5' => {
+    :mem  => 512,
+    :template_disk => '/work/libvirt-template-guest.img',
+    :disk => {
+      :path => '/work/libvirtd/images',
+      :name => 'n5.img'
+    },
+    :mac => '52:00:00:00:00:03'
   }
 }
