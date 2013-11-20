fba9efaa4d26cbcf13155e7e72c825a5929dd39b
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-11-20 23:29:34 +0400

    fix delete for defined domains

--- cookbooks/infra/recipes/delete-vm.rb
+++ cookbooks/infra/recipes/delete-vm.rb
@@ -3,8 +3,22 @@
 node.lvirt.vms.each do |name,info|
   vm_cfg = node.lvirt.vmdefault.deep_merge(info)
   if vm_cfg.deleted
-    execute "virsh #{ node.lvirt.delete_as[vm_cfg.produce_as] } #{ name }" do
+
+    execute "virsh undefine #{ name }" do
+      only_if "virsh dominfo n5 | grep Persistent: | grep -c yes"
+    end
+
+    execute "virsh destroy #{ name }" do
       only_if "virsh domid --domain #{ name }"
+    end
+
+    if !vm_cfg.disk.key?('name')
+      vm_cfg.update({ :disk => { :name => name + '.img' } })
+    end
+
+    file "#{ vm_cfg.disk.folder }/#{ vm_cfg.disk.name }" do
+      action :delete
+      backup false
     end
   end
 end
