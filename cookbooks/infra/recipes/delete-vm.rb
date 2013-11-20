e4c09dc6c6e6a99093821f2d2c1ea8893cbce876
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-11-20 22:34:17 +0400

    fix missing each at infra::delete-vm

--- cookbooks/infra/recipes/delete-vm.rb
+++ cookbooks/infra/recipes/delete-vm.rb
@@ -1,10 +1,10 @@
 
 
-node.lvirt.vms do |name,info|
+node.lvirt.vms.each do |name,info|
   vm_cfg = node.lvirt.vmdefault.deep_merge(info)
   if vm_cfg.deleted
     execute "virsh destroy #{ name }" do
-      not_if "virsh domid --domain #{ name }"
+      only_if "virsh domid --domain #{ name }"
     end
   end
 end
