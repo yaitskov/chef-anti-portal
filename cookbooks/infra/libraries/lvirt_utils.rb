fa9d832a72a8ee5ee4af2d85ad21bec204ec5c03
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-11-24 15:16:15 +0400

    fix inverse logic virt-resize status

--- cookbooks/infra/libraries/lvirt_utils.rb
+++ cookbooks/infra/libraries/lvirt_utils.rb
@@ -3,13 +3,19 @@
 def resize_raw_image(path,new_size_mb)
   tmpimage = Tempfile.new('resizing_' + File.basename(path), File.dirname(path))
   begin
-    tmpimage.truncate(new_size_mb * 1024 * 1024)
+    new_size = new_size_mb * 1024 * 1024
+    tmpimage.truncate(new_size)
+    #Chef::Log.info("virt-resize --expand /dev/sda1 #{path} #{tmpimage.path}")
+    #Chef::Log.info("new size #{new_size} old size  #{ File.size(path) }")    
     %x(virt-resize --expand /dev/sda1 #{path} #{tmpimage.path})
-    if !$?.exitstatus
+    e_st = $?.exitstatus
+    #Chef::Log.info("virt-resize exit status #{e_st}")        
+    if e_st != 0
       tmpimage.close
       tmpimage.unlink
       raise "failed resize"
     else
+      Chef::Log.info("resize is ok")      
       tmpimage.close
       File.unlink(path)
       File.rename(tmpimage.path, path)
