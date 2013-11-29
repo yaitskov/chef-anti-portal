3bd5cd72c1ce0a143b1d3de75405b19dc0035f02
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-11-30 02:10:02 +0400

    filter double node bootstrup

--- cookbooks/infra/recipes/chef-nodes.rb
+++ cookbooks/infra/recipes/chef-nodes.rb
@@ -4,17 +4,23 @@
 
 require 'chef/knife'
 
+repo_path = (File.dirname(__FILE__) + '/../../..')
+
+chef_nodes = %x(cd #{repo_path} ; knife node list)
+
 node.lvirt.vms.each do |name,info|
   zone = node.net.bind.zones.find { |zone| zone.xentries.key?(name) }
   host_name = zone.xentries[name]
-  %x(ping -c 1 #{ host_name }  )
-  if $?.exitstatus == 0
+  %x(ping -c 1 #{ host_name })
+  #puts "chef nodes: #{ chef_nodes }"
+  if $?.exitstatus == 0 and !chef_nodes.include?(name.to_s)
     ruby_block "bootstrap #{ name }" do
       block do
-        Dir.chdir (File.dirname(__FILE__) + '/../../..')
-        Chef::Knife.run(%W(bootstrap -i .chef/ssh-node-key.pem
+        Dir.chdir repo_path do
+          Chef::Knife.run(%W(bootstrap -i .chef/ssh-node-key.pem
                           -N #{name} --sudo -x #{node.root_user.name}
                           #{host_name}.#{zone.domain}))
+        end
       end
     end
   end
