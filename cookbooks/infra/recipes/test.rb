383e7c175340c132a542cbc2073e606aaa6fcb07
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-11-30 20:22:46 +0400

    demo access to fqdn, ip, chef node name attributes

--- cookbooks/infra/recipes/test.rb
+++ cookbooks/infra/recipes/test.rb
@@ -1,7 +1,19 @@
 
 
+node.languages.each do |lname,ldesc|
+  log "lang #{lname}"
+end
 
+log "node ipaddr =   #{node.ipaddress},  name = #{node.name}"
+log "node fqdn =   #{node.fqdn}"
 log "current option dd = #{ node.dd }"
+
+directory "/tmp/chef-#{Chef::Config[:node_name]}"
+
+Chef::Config.each do |k,v|
+  log " configs #{ k } => #{ v }"
+end
+log "all configs #{Chef::Config}"
 
 ruby_block "bbbb" do
   block do
