594459121eea55338b444a8f3e05aa2fe3ba220d
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-11-30 21:51:40 +0400

    add cassandra setting to anit-portal

--- cookbooks/anti-portal/attributes/default.rb
+++ cookbooks/anti-portal/attributes/default.rb
@@ -1,3 +1,15 @@
 override['java']['install_flavor'] = 'oracle'
 override['java']['jdk_version'] = '7'
 override['java']['oracle']['accept_oracle_download_terms'] = true
+
+
+
+override.cassandra = {
+  :cluster_name => 'AntiPortal',
+  :user => 'dan',
+  :rpc_address => '0.0.0.0',
+  :vnodes => 256,
+  :seeds => search(:node, 'tag:cassandra-seed').map {|n|n[:ipaddress]},
+  :snitch => 'SimpleSnitch'
+}
+
